47f23a8f6cf5ff561e235f38b92b51bc6c530591
munevverozhan <merveozh@gmail.com>
2022-08-11 21:23:25 +0300

    Projeye master page eklendi. TarifOner,Iletisim,Hakkimizda,GununYemegi ve AnaSayfa web  formları eklendi. Bağlantı class'ı oluşturulup veritabanı ile bağlantı sağlandı.

---
+++ YemekTarifiSite/AnaSayfa.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Kullanici.master" AutoEventWireup="true" CodeFile="AnaSayfa.aspx.cs" Inherits="AnaSayfa" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <p>
+    deneme test sayfası</p>
+</asp:Content>
+
---
+++ YemekTarifiSite/App_Code/sqlBaglantiClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+/// <summary>
+/// Summary description for sqlBaglantiClass
+/// </summary>
+public class sqlBaglantiClass
+{
+    public SqlConnection Baglanti()
+    {
+        SqlConnection conn = new SqlConnection(@"Data Source = DESKTOP-GBKS0E6; Initial Catalog = YemekTarifiSitesiDB; Integrated Security = True");
+        conn.Open();
+        return conn;
+    }
+}
